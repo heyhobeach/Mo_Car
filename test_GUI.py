56bfc34384360602174f047657faaf109f64e328
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2023-05-22 03:43:08 +0900

    유튜브 보고 따라 하기는 했으나 실행이 되지 않음  cvzone은 설치 한 상태 어느정도 실행이 된다면 mediapipe를 설치하여 눈의 자표를 받아와 이미지 위치를 조절할 계획임

--- test_GUI.py
+++ test_GUI.py
@@ -1,36 +1,41 @@
 from string import hexdigits
 
+import cvzone
 import cv2
 
 
-
 capture = cv2.VideoCapture(0)
-capture.set(cv2.CAP_PROP_FRAME_WIDTH,640)
-capture.set(cv2.CAP_PROP_FRAME_WIDTH,480)
+ret, frame = capture.read()
 
-while cv2.waitKey(33)<0:
+blue=(255,0,0)
+
+mirror_img=cv2.imread('Rear View Mirror.png',cv2.IMREAD_UNCHANGED)
+#mirror_img=cv2.resize(mirror_img,dsize=(100,70),interpolation=cv2.INTER_LINEAR)
+mirror_img=cv2.resize(mirror_img,(0,0),None,0.3,0.3)
+
+hf, wf,cf=mirror_img.shape
+hb,wb,cv=frame.shape
+
+while True:
     ret, frame = capture.read()
-    cv2.imshow("VideoFrame", frame)
+    img_result = cvzone.overlayPNG(frame,mirror_img,[0,hb-hf])
+
+    cv2.imshow("Image",img_result)
+    cv2.waitKey(1)
 
 
-
-if not capture.isOpened():
-    print("not open webcam")
-    exit()
-
-
-while capture.isOpend():
-    ret, frame = capture.read()
-
-    if ret:
-        cv2.imshow("VideoFrame",frame)
-
-    if cv2.waitKey(1) & 0xFF ==ord('q'):
-        break
-
-
-
-
+    #cv2.imshow("VideoFrame", frame)
+    #img =cv2.rectangle(frame,(10,10),(100,100),blue,3)
+    
+    
+    #print(img.shape)
+    
+    #cv2.imshow("videoFrame", resized_img)
+    #c=cv2.waitKey(1)
+    #if c==27:
+    #    break
 
 capture.release()
 cv2.destroyAllWindows()
+
+print('hello')

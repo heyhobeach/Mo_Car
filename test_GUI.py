607c61bb225490d1318fec12b80bc81892633130
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2023-05-26 10:36:47 +0900

    Update test_GUI.py
    최신버전 v.00001

--- test_GUI.py
+++ test_GUI.py
@@ -4,38 +4,38 @@ import cvzone
 import cv2
 
 
-capture = cv2.VideoCapture(0)
-ret, frame = capture.read()
+cap = cv2.VideoCapture(0)
+success, img = cap.read()
 
-blue=(255,0,0)
 
-mirror_img=cv2.imread('Rear View Mirror.png',cv2.IMREAD_UNCHANGED)
-#mirror_img=cv2.resize(mirror_img,dsize=(100,70),interpolation=cv2.INTER_LINEAR)
-mirror_img=cv2.resize(mirror_img,(0,0),None,0.3,0.3)
+imgFront=cv2.imread('Rear View Mirror.png',cv2.IMREAD_UNCHANGED)
+imgFront=cv2.resize(imgFront,(0,0),None,0.05,0.05)
 
-hf, wf,cf=mirror_img.shape
-hb,wb,cv=frame.shape
+
+
+hf, wf,cf=imgFront.shape#현재 크기 받아옴 쉽게 생각하면 너비 예를들어 720x1280 3채널 같은 정보 받아옴 rear view mirror 이미지
+hb,wb,cb=img.shape#현재 크기 받아옴 쉽게 생각하면 너비 예를들어 720x1280 3채널 같은 정보 받아옴
+
+
+
+print("hf: %f , wf: %f ,cf: %f"%(hf,wf,cf))#정보 출력
+print("hb: %f , wb: %f ,cb: %f"%(hb,wb,cb))
 
 while True:
-    ret, frame = capture.read()
-    img_result = cvzone.overlayPNG(frame,mirror_img,[0,hb-hf])
+    success, img = cap.read()
+    #img=cv2.cvtColor(img,cv2.COLOR_BGR2BGRA) 해당 부분을 지워야 화면이 나옴
+    imgFront=cv2.cvtColor(imgFront,cv2.COLOR_BGR2BGRA)
+    imgResult = cvzone.overlayPNG(img,imgFront,[0, hb-hf])# 해당 부분은 있어야함
 
-    cv2.imshow("Image",img_result)
-    cv2.waitKey(1)
 
-
-    #cv2.imshow("VideoFrame", frame)
-    #img =cv2.rectangle(frame,(10,10),(100,100),blue,3)
+    cv2.imshow("Image",imgResult)
+    imgResult=cv2.cvtColor(imgFront,cv2.COLOR_BGR2BGRA)
     
+    if cv2.waitKey(0):
+        break
     
-    #print(img.shape)
-    
-    #cv2.imshow("videoFrame", resized_img)
-    #c=cv2.waitKey(1)
-    #if c==27:
-    #    break
 
-capture.release()
+
+
+cap.release()
 cv2.destroyAllWindows()
-
-print('hello')
